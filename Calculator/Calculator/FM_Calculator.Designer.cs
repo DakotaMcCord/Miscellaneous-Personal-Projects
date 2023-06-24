a854372a6e514ddd109b9b007e9d1b7015b8d660
DakotaMcCord <107652695+DakotaMcCord@users.noreply.github.com>
2023-06-24 13:21:03 -0500

    Add files via upload

---
+++ Calculator/Calculator/FM_Calculator.Designer.cs
@@ -0,0 +1,354 @@
+﻿
+namespace Calculator {
+    partial class FM_Calculator {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.PNL_Numberpad = new System.Windows.Forms.Panel();
+            this.BTN_PLeft = new System.Windows.Forms.Button();
+            this.BTN_PRight = new System.Windows.Forms.Button();
+            this.BTN_Enter = new System.Windows.Forms.Button();
+            this.BTN_Clear = new System.Windows.Forms.Button();
+            this.BTN_Plus = new System.Windows.Forms.Button();
+            this.BTN_Back = new System.Windows.Forms.Button();
+            this.BTN_Zero = new System.Windows.Forms.Button();
+            this.BTN_Point = new System.Windows.Forms.Button();
+            this.BTN_Minus = new System.Windows.Forms.Button();
+            this.BTN_Three = new System.Windows.Forms.Button();
+            this.BTN_Two = new System.Windows.Forms.Button();
+            this.BTN_One = new System.Windows.Forms.Button();
+            this.BTN_Times = new System.Windows.Forms.Button();
+            this.Btn_Six = new System.Windows.Forms.Button();
+            this.BTN_Five = new System.Windows.Forms.Button();
+            this.BTN_Four = new System.Windows.Forms.Button();
+            this.BTN_Division = new System.Windows.Forms.Button();
+            this.BTN_Nine = new System.Windows.Forms.Button();
+            this.BTN_Eight = new System.Windows.Forms.Button();
+            this.BTN_Seven = new System.Windows.Forms.Button();
+            this.TBX_MathView = new System.Windows.Forms.TextBox();
+            this.PNL_Numberpad.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // PNL_Numberpad
+            // 
+            this.PNL_Numberpad.Controls.Add(this.BTN_PLeft);
+            this.PNL_Numberpad.Controls.Add(this.BTN_PRight);
+            this.PNL_Numberpad.Controls.Add(this.BTN_Enter);
+            this.PNL_Numberpad.Controls.Add(this.BTN_Clear);
+            this.PNL_Numberpad.Controls.Add(this.BTN_Plus);
+            this.PNL_Numberpad.Controls.Add(this.BTN_Back);
+            this.PNL_Numberpad.Controls.Add(this.BTN_Zero);
+            this.PNL_Numberpad.Controls.Add(this.BTN_Point);
+            this.PNL_Numberpad.Controls.Add(this.BTN_Minus);
+            this.PNL_Numberpad.Controls.Add(this.BTN_Three);
+            this.PNL_Numberpad.Controls.Add(this.BTN_Two);
+            this.PNL_Numberpad.Controls.Add(this.BTN_One);
+            this.PNL_Numberpad.Controls.Add(this.BTN_Times);
+            this.PNL_Numberpad.Controls.Add(this.Btn_Six);
+            this.PNL_Numberpad.Controls.Add(this.BTN_Five);
+            this.PNL_Numberpad.Controls.Add(this.BTN_Four);
+            this.PNL_Numberpad.Controls.Add(this.BTN_Division);
+            this.PNL_Numberpad.Controls.Add(this.BTN_Nine);
+            this.PNL_Numberpad.Controls.Add(this.BTN_Eight);
+            this.PNL_Numberpad.Controls.Add(this.BTN_Seven);
+            this.PNL_Numberpad.Location = new System.Drawing.Point(12, 116);
+            this.PNL_Numberpad.Name = "PNL_Numberpad";
+            this.PNL_Numberpad.Size = new System.Drawing.Size(260, 183);
+            this.PNL_Numberpad.TabIndex = 0;
+            // 
+            // BTN_PLeft
+            // 
+            this.BTN_PLeft.Location = new System.Drawing.Point(3, 147);
+            this.BTN_PLeft.Name = "BTN_PLeft";
+            this.BTN_PLeft.Size = new System.Drawing.Size(29, 30);
+            this.BTN_PLeft.TabIndex = 19;
+            this.BTN_PLeft.TabStop = false;
+            this.BTN_PLeft.Text = "(";
+            this.BTN_PLeft.UseVisualStyleBackColor = true;
+            this.BTN_PLeft.Click += new System.EventHandler(this.BTN_PLeft_Click);
+            // 
+            // BTN_PRight
+            // 
+            this.BTN_PRight.Location = new System.Drawing.Point(33, 147);
+            this.BTN_PRight.Name = "BTN_PRight";
+            this.BTN_PRight.Size = new System.Drawing.Size(30, 30);
+            this.BTN_PRight.TabIndex = 18;
+            this.BTN_PRight.TabStop = false;
+            this.BTN_PRight.Text = ")";
+            this.BTN_PRight.UseVisualStyleBackColor = true;
+            this.BTN_PRight.Click += new System.EventHandler(this.BTN_PRight_Click);
+            // 
+            // BTN_Enter
+            // 
+            this.BTN_Enter.Location = new System.Drawing.Point(165, 147);
+            this.BTN_Enter.Name = "BTN_Enter";
+            this.BTN_Enter.Size = new System.Drawing.Size(92, 30);
+            this.BTN_Enter.TabIndex = 17;
+            this.BTN_Enter.TabStop = false;
+            this.BTN_Enter.Text = "Enter";
+            this.BTN_Enter.UseVisualStyleBackColor = true;
+            this.BTN_Enter.Click += new System.EventHandler(this.BTN_Enter_Click);
+            // 
+            // BTN_Clear
+            // 
+            this.BTN_Clear.Location = new System.Drawing.Point(67, 147);
+            this.BTN_Clear.Name = "BTN_Clear";
+            this.BTN_Clear.Size = new System.Drawing.Size(94, 30);
+            this.BTN_Clear.TabIndex = 16;
+            this.BTN_Clear.TabStop = false;
+            this.BTN_Clear.Text = "Clear";
+            this.BTN_Clear.UseVisualStyleBackColor = true;
+            this.BTN_Clear.Click += new System.EventHandler(this.BTN_Clear_Click);
+            // 
+            // BTN_Plus
+            // 
+            this.BTN_Plus.Location = new System.Drawing.Point(197, 111);
+            this.BTN_Plus.Name = "BTN_Plus";
+            this.BTN_Plus.Size = new System.Drawing.Size(60, 30);
+            this.BTN_Plus.TabIndex = 15;
+            this.BTN_Plus.TabStop = false;
+            this.BTN_Plus.Text = "( + )";
+            this.BTN_Plus.UseVisualStyleBackColor = true;
+            this.BTN_Plus.Click += new System.EventHandler(this.BTN_Plus_Click);
+            // 
+            // BTN_Back
+            // 
+            this.BTN_Back.Location = new System.Drawing.Point(131, 111);
+            this.BTN_Back.Name = "BTN_Back";
+            this.BTN_Back.Size = new System.Drawing.Size(60, 30);
+            this.BTN_Back.TabIndex = 14;
+            this.BTN_Back.TabStop = false;
+            this.BTN_Back.Text = "<-";
+            this.BTN_Back.UseVisualStyleBackColor = true;
+            this.BTN_Back.Click += new System.EventHandler(this.BTN_Back_Click);
+            // 
+            // BTN_Zero
+            // 
+            this.BTN_Zero.Location = new System.Drawing.Point(67, 111);
+            this.BTN_Zero.Name = "BTN_Zero";
+            this.BTN_Zero.Size = new System.Drawing.Size(60, 30);
+            this.BTN_Zero.TabIndex = 13;
+            this.BTN_Zero.TabStop = false;
+            this.BTN_Zero.Text = "0";
+            this.BTN_Zero.UseVisualStyleBackColor = true;
+            this.BTN_Zero.Click += new System.EventHandler(this.BTN_Zero_Click);
+            // 
+            // BTN_Point
+            // 
+            this.BTN_Point.Location = new System.Drawing.Point(3, 111);
+            this.BTN_Point.Name = "BTN_Point";
+            this.BTN_Point.Size = new System.Drawing.Size(60, 30);
+            this.BTN_Point.TabIndex = 12;
+            this.BTN_Point.TabStop = false;
+            this.BTN_Point.Text = "( . )";
+            this.BTN_Point.UseVisualStyleBackColor = true;
+            this.BTN_Point.Click += new System.EventHandler(this.BTN_Point_Click);
+            // 
+            // BTN_Minus
+            // 
+            this.BTN_Minus.Location = new System.Drawing.Point(197, 75);
+            this.BTN_Minus.Name = "BTN_Minus";
+            this.BTN_Minus.Size = new System.Drawing.Size(60, 30);
+            this.BTN_Minus.TabIndex = 11;
+            this.BTN_Minus.TabStop = false;
+            this.BTN_Minus.Text = "( - )";
+            this.BTN_Minus.UseVisualStyleBackColor = true;
+            this.BTN_Minus.Click += new System.EventHandler(this.BTN_Minus_Click);
+            // 
+            // BTN_Three
+            // 
+            this.BTN_Three.Location = new System.Drawing.Point(131, 75);
+            this.BTN_Three.Name = "BTN_Three";
+            this.BTN_Three.Size = new System.Drawing.Size(60, 30);
+            this.BTN_Three.TabIndex = 10;
+            this.BTN_Three.TabStop = false;
+            this.BTN_Three.Text = "3";
+            this.BTN_Three.UseVisualStyleBackColor = true;
+            this.BTN_Three.Click += new System.EventHandler(this.BTN_Three_Click);
+            // 
+            // BTN_Two
+            // 
+            this.BTN_Two.Location = new System.Drawing.Point(67, 75);
+            this.BTN_Two.Name = "BTN_Two";
+            this.BTN_Two.Size = new System.Drawing.Size(60, 30);
+            this.BTN_Two.TabIndex = 9;
+            this.BTN_Two.TabStop = false;
+            this.BTN_Two.Text = "2";
+            this.BTN_Two.UseVisualStyleBackColor = true;
+            this.BTN_Two.Click += new System.EventHandler(this.BTN_Two_Click);
+            // 
+            // BTN_One
+            // 
+            this.BTN_One.Location = new System.Drawing.Point(3, 75);
+            this.BTN_One.Name = "BTN_One";
+            this.BTN_One.Size = new System.Drawing.Size(60, 30);
+            this.BTN_One.TabIndex = 8;
+            this.BTN_One.TabStop = false;
+            this.BTN_One.Text = "1";
+            this.BTN_One.UseVisualStyleBackColor = true;
+            this.BTN_One.Click += new System.EventHandler(this.BTN_One_Click);
+            // 
+            // BTN_Times
+            // 
+            this.BTN_Times.Location = new System.Drawing.Point(197, 39);
+            this.BTN_Times.Name = "BTN_Times";
+            this.BTN_Times.Size = new System.Drawing.Size(60, 30);
+            this.BTN_Times.TabIndex = 7;
+            this.BTN_Times.TabStop = false;
+            this.BTN_Times.Text = "( * )";
+            this.BTN_Times.UseVisualStyleBackColor = true;
+            this.BTN_Times.Click += new System.EventHandler(this.BTN_Times_Click);
+            // 
+            // Btn_Six
+            // 
+            this.Btn_Six.Location = new System.Drawing.Point(131, 39);
+            this.Btn_Six.Name = "Btn_Six";
+            this.Btn_Six.Size = new System.Drawing.Size(60, 30);
+            this.Btn_Six.TabIndex = 6;
+            this.Btn_Six.TabStop = false;
+            this.Btn_Six.Text = "6";
+            this.Btn_Six.UseVisualStyleBackColor = true;
+            this.Btn_Six.Click += new System.EventHandler(this.Btn_Six_Click);
+            // 
+            // BTN_Five
+            // 
+            this.BTN_Five.Location = new System.Drawing.Point(67, 39);
+            this.BTN_Five.Name = "BTN_Five";
+            this.BTN_Five.Size = new System.Drawing.Size(60, 30);
+            this.BTN_Five.TabIndex = 5;
+            this.BTN_Five.TabStop = false;
+            this.BTN_Five.Text = "5";
+            this.BTN_Five.UseVisualStyleBackColor = true;
+            this.BTN_Five.Click += new System.EventHandler(this.BTN_Five_Click);
+            // 
+            // BTN_Four
+            // 
+            this.BTN_Four.Location = new System.Drawing.Point(3, 39);
+            this.BTN_Four.Name = "BTN_Four";
+            this.BTN_Four.Size = new System.Drawing.Size(60, 30);
+            this.BTN_Four.TabIndex = 4;
+            this.BTN_Four.TabStop = false;
+            this.BTN_Four.Text = "4";
+            this.BTN_Four.UseVisualStyleBackColor = true;
+            this.BTN_Four.Click += new System.EventHandler(this.BTN_Four_Click);
+            // 
+            // BTN_Division
+            // 
+            this.BTN_Division.Location = new System.Drawing.Point(197, 3);
+            this.BTN_Division.Name = "BTN_Division";
+            this.BTN_Division.Size = new System.Drawing.Size(60, 30);
+            this.BTN_Division.TabIndex = 3;
+            this.BTN_Division.TabStop = false;
+            this.BTN_Division.Text = "( / )";
+            this.BTN_Division.UseVisualStyleBackColor = true;
+            this.BTN_Division.Click += new System.EventHandler(this.BTN_Division_Click);
+            // 
+            // BTN_Nine
+            // 
+            this.BTN_Nine.Location = new System.Drawing.Point(131, 3);
+            this.BTN_Nine.Name = "BTN_Nine";
+            this.BTN_Nine.Size = new System.Drawing.Size(60, 30);
+            this.BTN_Nine.TabIndex = 2;
+            this.BTN_Nine.TabStop = false;
+            this.BTN_Nine.Text = "9";
+            this.BTN_Nine.UseVisualStyleBackColor = true;
+            this.BTN_Nine.Click += new System.EventHandler(this.BTN_Nine_Click);
+            // 
+            // BTN_Eight
+            // 
+            this.BTN_Eight.Location = new System.Drawing.Point(67, 3);
+            this.BTN_Eight.Name = "BTN_Eight";
+            this.BTN_Eight.Size = new System.Drawing.Size(60, 30);
+            this.BTN_Eight.TabIndex = 1;
+            this.BTN_Eight.TabStop = false;
+            this.BTN_Eight.Text = "8";
+            this.BTN_Eight.UseVisualStyleBackColor = true;
+            this.BTN_Eight.Click += new System.EventHandler(this.BTN_Eight_Click);
+            // 
+            // BTN_Seven
+            // 
+            this.BTN_Seven.Location = new System.Drawing.Point(3, 3);
+            this.BTN_Seven.Name = "BTN_Seven";
+            this.BTN_Seven.Size = new System.Drawing.Size(60, 30);
+            this.BTN_Seven.TabIndex = 0;
+            this.BTN_Seven.TabStop = false;
+            this.BTN_Seven.Text = "7";
+            this.BTN_Seven.UseVisualStyleBackColor = true;
+            this.BTN_Seven.Click += new System.EventHandler(this.BTN_Seven_Click);
+            // 
+            // TBX_MathView
+            // 
+            this.TBX_MathView.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.TBX_MathView.Location = new System.Drawing.Point(12, 12);
+            this.TBX_MathView.Multiline = true;
+            this.TBX_MathView.Name = "TBX_MathView";
+            this.TBX_MathView.Size = new System.Drawing.Size(260, 98);
+            this.TBX_MathView.TabIndex = 1;
+            this.TBX_MathView.MouseClick += new System.Windows.Forms.MouseEventHandler(this.TBX_MathView_MouseClick);
+            this.TBX_MathView.TextChanged += new System.EventHandler(this.TBX_MathView_TextChanged);
+            this.TBX_MathView.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TBX_MathView_KeyDown);
+            // 
+            // FM_Calculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(284, 311);
+            this.Controls.Add(this.TBX_MathView);
+            this.Controls.Add(this.PNL_Numberpad);
+            this.Name = "FM_Calculator";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FM_Calculator_Load);
+            this.PNL_Numberpad.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PNL_Numberpad;
+        private System.Windows.Forms.TextBox TBX_MathView;
+        private System.Windows.Forms.Button BTN_Division;
+        private System.Windows.Forms.Button BTN_Nine;
+        private System.Windows.Forms.Button BTN_Eight;
+        private System.Windows.Forms.Button BTN_Seven;
+        private System.Windows.Forms.Button BTN_Clear;
+        private System.Windows.Forms.Button BTN_Plus;
+        private System.Windows.Forms.Button BTN_Back;
+        private System.Windows.Forms.Button BTN_Zero;
+        private System.Windows.Forms.Button BTN_Point;
+        private System.Windows.Forms.Button BTN_Minus;
+        private System.Windows.Forms.Button BTN_Three;
+        private System.Windows.Forms.Button BTN_Two;
+        private System.Windows.Forms.Button BTN_One;
+        private System.Windows.Forms.Button BTN_Times;
+        private System.Windows.Forms.Button Btn_Six;
+        private System.Windows.Forms.Button BTN_Five;
+        private System.Windows.Forms.Button BTN_Four;
+        private System.Windows.Forms.Button BTN_Enter;
+        private System.Windows.Forms.Button BTN_PLeft;
+        private System.Windows.Forms.Button BTN_PRight;
+    }
+}
+
---
+++ Calculator/Calculator/FM_Calculator.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator {
+    public partial class FM_Calculator : Form {
+        #region Core
+        public FM_Calculator() {
+            InitializeComponent();
+        }
+
+        List<Keys> AllowKeys = new List<Keys>();
+        int caretintex, MTxtLength = 30;
+
+        private void FM_Calculator_Load(object sender, EventArgs e) {
+            this.ControlBox = true;
+            this.Text = "Calculator";
+            this.Height = 350;
+            this.Width = 300;
+            this.MaximumSize = this.Size;
+            this.FormBorderStyle = FormBorderStyle.Fixed3D;
+            this.BackColor = Color.FromArgb(255, 100, 100, 100);
+            this.ShowIcon = false;
+
+            PNL_Numberpad.BackColor = Color.FromArgb(255, 150, 150, 150);
+            PNL_Numberpad.ForeColor = Color.Black;
+
+            TBX_MathView.BackColor = Color.Silver;
+            TBX_MathView.Font = new Font("Comic Sans MS", 20f, FontStyle.Regular, GraphicsUnit.Point);
+
+            #region AllowKeys
+            //AllowKeys.Add(Keys.Enter);
+            AllowKeys.Add(Keys.Back);
+            AllowKeys.Add(Keys.OemPeriod);
+            AllowKeys.Add(Keys.Multiply);
+            AllowKeys.Add(Keys.OemQuestion);
+            AllowKeys.Add(Keys.Oemplus);
+            AllowKeys.Add(Keys.OemMinus);
+
+            AllowKeys.Add(Keys.Up);
+            AllowKeys.Add(Keys.Left);
+            AllowKeys.Add(Keys.Down);
+            AllowKeys.Add(Keys.Right);
+
+            AllowKeys.Add(Keys.D0);
+            AllowKeys.Add(Keys.NumPad0);
+            AllowKeys.Add(Keys.D1);
+            AllowKeys.Add(Keys.NumPad1);
+            AllowKeys.Add(Keys.D2);
+            AllowKeys.Add(Keys.NumPad2);
+            AllowKeys.Add(Keys.D3);
+            AllowKeys.Add(Keys.NumPad3);
+            AllowKeys.Add(Keys.D4);
+            AllowKeys.Add(Keys.NumPad4);
+            AllowKeys.Add(Keys.D5);
+            AllowKeys.Add(Keys.NumPad5);
+            AllowKeys.Add(Keys.D6);
+            AllowKeys.Add(Keys.NumPad6);
+            AllowKeys.Add(Keys.D7);
+            AllowKeys.Add(Keys.NumPad7);
+            AllowKeys.Add(Keys.D8);
+            AllowKeys.Add(Keys.NumPad8);
+            AllowKeys.Add(Keys.D9);
+            AllowKeys.Add(Keys.NumPad9);
+            #endregion
+        } // Load Form
+        private void TBX_MathView_KeyDown(object sender, KeyEventArgs e) {
+            Keys K = e.KeyCode;
+            if (K == Keys.Enter) {
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+                DotheMath();
+            } else if (K == Keys.C) {
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+                ClearBox();
+            } else if (!AllowKeys.Contains(K)) {
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+            }
+
+            UpdateCaret();
+        } // Keydown Events - Disable invalid Keys
+        private void TBX_MathView_TextChanged(object sender, EventArgs e) {
+            if(TBX_MathView.Text.Contains("_")) {
+                TBX_MathView.Text = TBX_MathView.Text.Replace("_", "-");
+                TBX_MathView.Select(TBX_MathView.Text.Length, 0);
+            }
+            if (TBX_MathView.Text.Contains("=")) {
+                TBX_MathView.Text = TBX_MathView.Text.Replace("=", "+");
+                TBX_MathView.Select(TBX_MathView.Text.Length, 0);
+            }
+            if (TBX_MathView.Text.Contains("?")) {
+                TBX_MathView.Text = TBX_MathView.Text.Replace("?", "/");
+                TBX_MathView.Select(TBX_MathView.Text.Length, 0);
+            }
+        } // Text Changed
+        private void TBX_MathView_MouseClick(object sender, MouseEventArgs e) {
+            UpdateCaret();
+        } // Mous Click event on box
+
+
+
+        public void DotheMath() {
+            // P E M D A S
+            string Problem = TBX_MathView.Text;
+
+            try {
+                DataTable table = new DataTable();
+                var ProblemOutput = table.Compute(Problem, "");
+
+                TBX_MathView.Text = ProblemOutput.ToString();
+                TBX_MathView.Select(TBX_MathView.Text.Length, 0);
+            }
+            catch {
+                MessageBox.Show(
+                    $"An Unexpected error has ocured. {Environment.NewLine}" +
+                    $"Please verify there are no typos.", "Err", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+
+
+
+
+
+        }
+        #endregion
+        #region Special
+        private void BTN_Back_Click(object sender, EventArgs e) {
+            if (TBX_MathView.Text != string.Empty) {
+                TBX_MathView.Text = TBX_MathView.Text.Remove(TBX_MathView.Text.Length - 1, 1);
+                TBX_MathView.Select(TBX_MathView.Text.Length, 0);
+            }
+            SelectCalcView();
+        } // Back
+        private void BTN_Clear_Click(object sender, EventArgs e) {
+            ClearBox();
+        }// Clear
+                    public void ClearBox() {
+                        TBX_MathView.Text = string.Empty;
+                        SelectCalcView();
+                    } // Clears Box
+        private void BTN_Point_Click(object sender, EventArgs e) {
+            TBX_MathView.Text = TBX_MathView.Text.Insert(TBX_MathView.SelectionStart, ".");
+            SelectCalcView();
+        } // Point
+        private void BTN_PLeft_Click(object sender, EventArgs e) {
+            TBX_MathView.Text = TBX_MathView.Text.Insert(TBX_MathView.SelectionStart, "(");
+            SelectCalcView();
+        } // Pleft
+        private void BTN_PRight_Click(object sender, EventArgs e) {
+            TBX_MathView.Text = TBX_MathView.Text.Insert(TBX_MathView.SelectionStart, ")");
+            SelectCalcView();
+        } // Pright
+
+        private void BTN_Enter_Click(object sender, EventArgs e) {
+            DotheMath();
+            SelectCalcView();
+        }
+        #endregion
+        #region Numbers
+        private void BTN_Zero_Click(object sender, EventArgs e) {
+            TBX_MathView.Text = TBX_MathView.Text.Insert(TBX_MathView.SelectionStart, "0");
+            SelectCalcView();
+        }
+        private void BTN_One_Click(object sender, EventArgs e) {
+            TBX_MathView.Text = TBX_MathView.Text.Insert(TBX_MathView.SelectionStart, "1");
+            SelectCalcView();
+        }
+        private void BTN_Two_Click(object sender, EventArgs e) {
+            TBX_MathView.Text = TBX_MathView.Text.Insert(TBX_MathView.SelectionStart, "2");
+            SelectCalcView();
+        }
+        private void BTN_Three_Click(object sender, EventArgs e) {
+            TBX_MathView.Text = TBX_MathView.Text.Insert(TBX_MathView.SelectionStart, "3");
+            SelectCalcView();
+        }
+        private void BTN_Four_Click(object sender, EventArgs e) {
+            TBX_MathView.Text = TBX_MathView.Text.Insert(TBX_MathView.SelectionStart, "4");
+            SelectCalcView();
+        }
+        private void BTN_Five_Click(object sender, EventArgs e) {
+            TBX_MathView.Text = TBX_MathView.Text.Insert(TBX_MathView.SelectionStart, "5");
+            SelectCalcView();
+        }
+        private void Btn_Six_Click(object sender, EventArgs e) {
+            TBX_MathView.Text = TBX_MathView.Text.Insert(TBX_MathView.SelectionStart, "6");
+            SelectCalcView();
+        }
+        private void BTN_Seven_Click(object sender, EventArgs e) {
+            TBX_MathView.Text = TBX_MathView.Text.Insert(TBX_MathView.SelectionStart, "7");
+            SelectCalcView();
+        }
+        private void BTN_Eight_Click(object sender, EventArgs e) {
+            TBX_MathView.Text = TBX_MathView.Text.Insert(TBX_MathView.SelectionStart, "8");
+            SelectCalcView();
+        }
+        private void BTN_Nine_Click(object sender, EventArgs e) {
+            TBX_MathView.Text = TBX_MathView.Text.Insert(TBX_MathView.SelectionStart, "9");
+            SelectCalcView();
+        }
+        #endregion
+        #region Operations
+        private void BTN_Plus_Click(object sender, EventArgs e) {
+            TBX_MathView.Text = TBX_MathView.Text.Insert(TBX_MathView.SelectionStart, "+");
+            SelectCalcView();
+        }
+        private void BTN_Minus_Click(object sender, EventArgs e) {
+            TBX_MathView.Text = TBX_MathView.Text.Insert(TBX_MathView.SelectionStart, "-");
+            SelectCalcView();
+        }
+        private void BTN_Times_Click(object sender, EventArgs e) {
+            TBX_MathView.Text = TBX_MathView.Text.Insert(TBX_MathView.SelectionStart, "*");
+            SelectCalcView();
+        }
+        private void BTN_Division_Click(object sender, EventArgs e) {
+            TBX_MathView.Text = TBX_MathView.Text.Insert(TBX_MathView.SelectionStart, "/");
+            SelectCalcView();
+        }
+        #endregion
+        #region UpdateStuff
+        public void UpdateCaret() {
+            caretintex = TBX_MathView.SelectionStart;
+        } // Update Caret Position
+        public void SelectCalcView() {
+            TBX_MathView.Focus();
+            TBX_MathView.DeselectAll();
+            caretintex++;
+            TBX_MathView.Select(caretintex, 0);
+            
+        } // Slect Textbox View
+        //public bool StringTooLong() {
+        //    if (TBX_MathView.Text.Length >= MTxtLength) {
+        //        return true;
+        //    }
+        //    else { return false; }
+        //}
+        #endregion
+    }
+}
